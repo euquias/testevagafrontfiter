d0ee6c076adc6d00d945294d3f8755cec8abfc61
euquias <willmagalhaes25@gmail.com>
2022-02-25 14:18:00 -0300

    adicionando logo

--- testefiter/src/components/template/Logo.jsx
+++ testefiter/src/components/template/Logo.jsx
@@ -1,7 +1,10 @@
 import './Logo.css'
+import Logo from '../../assets/imgs/logo.png'
 import React from 'react'
 
 export default props =>
 <aside className='logo'>
-    logo
+<a href="/"className='logo'>
+    <img src={Logo} alt='logo'/>
+</a>
 </aside>
