c3b0ccf76863c41b3b1f649cd51223d116727594
euquias <willmagalhaes25@gmail.com>
2022-02-25 14:42:38 -0300

    adicionando footer

--- testefiter/src/components/template/Footer.jsx
+++ testefiter/src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 
 export default props =>
 <footer className='footer' >
-    rodapé
+    <span>
+       <h4>teste Fiter: Desenvolvido por Euquias Araujo</h4> 
+    </span>
 </footer>
